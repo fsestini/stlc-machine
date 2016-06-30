7c93e1d1f9bf112937116fecb349b54294ec3851
Filippo Sestini <sestini.filippo@gmail.com>
2016-06-30 22:44:01 +0200

    Update aux lambda-terms in Lambda.hs

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -98,7 +98,8 @@ false = Abstr "x" (Abstr "y" (Var "y"))
 ifThenElse = Abstr "b" (Abstr "m" (Abstr "n" (Appl (Appl (Var "b") (Var "m")) (Var "n"))))
 identity = Abstr "x" (Var "x")
 
-isZero = Abstr "n" (Appl (Var "n") true)
+isZero = Abstr "n" $ (Var "n") `Appl` (Abstr "w" $ Abstr "x" $ Abstr "y" (Var "y"))
+                               `Appl` (Abstr "x" $ Abstr "y" (Var "x"))
 
 sumF = Abstr "f" (Abstr "x" (Abstr "y"
     (ifThenElse `Appl` (Appl isZero (Var "y"))
@@ -110,16 +111,28 @@ lambdaSum = Appl yCombinator sumF
 
 -- fact = Abstr "f" (Abstr "n" (ifThenElse `Appl` (Appl isZero (Var "n")) `Appl` (numeral 1) `Appl` (Appl mult (Appl (Var "f") (Appl sub (Var "n") (numeral 1))))))
 
-pair m n = Abstr "x" (Appl (Appl (Var "x") m) n)
-first = Abstr "p" (Appl (Var "p") true)
-second = Abstr "p" (Appl (Var "p") false)
+kComb :: LambdaTerm String
+kComb = Abstr "x" (Abstr "y" (Var "x"))
 
-numeral :: Int -> LambdaTerm
-numeral 0 = identity
-numeral n = pair false (numeral (n-1))
+-- pair m n = Abstr "x" (Appl (Appl (Var "x") m) n)
+-- first = Abstr "p" (Appl (Var "p") true)
+-- second = Abstr "p" (Appl (Var "p") false)
 
-successor = Abstr "n" (pair false (Var "n"))
-predecessor = Abstr "n" (Appl second (Var "n"))
+numeral :: Int -> LambdaTerm String
+numeral 0 = Abstr "y" (Abstr "x" (Var "x"))
+numeral n = Abstr "y" (Abstr "x" (Appl (Var "y")
+                                 (Appl (Appl (numeral (n-1)) (Var "y")) (Var "x"))))
+
+successor :: LambdaTerm String
+successor = Abstr "n" $ Abstr "y" $ Abstr "x" $
+            (Var "y") `Appl` ((Var "n") `Appl` (Var "y") `Appl` (Var "x"))
+
+predecessor :: LambdaTerm String
+predecessor = Abstr "n" $ Abstr "s" $ Abstr "z" $
+              (Var "n") `Appl` (Abstr "p" $ Abstr "q" $
+                                 (Var "q") `Appl` (Appl (Var "p") (Var "s")))
+                        `Appl` (Abstr "x" (Var "z"))
+                        `Appl` (Abstr "x" (Var "x"))
 
 omega = Abstr "x" (Appl (Var "x") (Var "x"))
 bigOmega = Appl omega omega
